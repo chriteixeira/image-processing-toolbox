aeefd65ffb81dc650e0ced60d0b8a02b44a2ccf5
Christiano Teixeira <chriteixeira@gmail.com>
2016-07-19 22:16:30 -0300

    Adding inverse and fixed image saving (WIP)

--- index.js
+++ index.js
@@ -7,6 +7,31 @@ var ImageToolbox = function(){
 
 };
 
+ImageToolbox.prototype.invert() = function(imagePath){
+    fs.createReadStream(imagePath)
+        .pipe(new PNG({
+            filterType: 4
+        }))
+        .on('parsed', function() {
+
+            for (var y = 0; y < this.height; y++) {
+                for (var x = 0; x < this.width; x++) {
+                    var idx = (this.width * y + x) << 2;
+
+                    // invert color
+                    this.data[idx] = 255 - this.data[idx];
+                    this.data[idx+1] = 255 - this.data[idx+1];
+                    this.data[idx+2] = 255 - this.data[idx+2];
+
+                    // and reduce opacity
+                    this.data[idx+3] = this.data[idx+3] >> 1;
+                }
+            }
+            console.log(this);
+            this.pack().pipe(fs.createWriteStream('out.png'));
+        });
+};
+
 ImageToolbox.prototype.imageDiff = function(image1Path, image2Path){
     var file1 = fs.readFileSync(image1Path);
     var file2 = fs.readFileSync(image2Path);
@@ -20,8 +45,10 @@ ImageToolbox.prototype.imageDiff = function(image1Path, image2Path){
             console.log(image1Data);
             console.log(image2Data);
 
-            var result = new PNG({width: image1Data.width, heigth: image1Data.heigth});
-            result.data = [];
+            var result = new PNG({width: image1Data.width, height: image1Data.height});
+            var resultData = [];
+
+            console.log(image1Data.frames[0].data.length);
 
             for(var i=0; i < image1Data.frames[0].data.length; i+=4){
                 var r1 = image1Data.frames[0].data[i];
@@ -36,21 +63,21 @@ ImageToolbox.prototype.imageDiff = function(image1Path, image2Path){
                 if(r1 !== r2 || 
                     b1 !== b2 ||
                     r1 !== r2){
-                        result.data[i] = 0xff;
-                        result.data[i+1] = 0xff;
-                        result.data[i+2] = 0xff;
-                        result.data[i+3] = 0xff;
+                        resultData[i] = 0x00;
+                        resultData[i+1] = 0xff;
+                        resultData[i+2] = 0xff;
+                        resultData[i+3] = 0x00;
                 }
                 else{
-                    result.data[i] = 0xff;
-                    result.data[i+1] = 0xff;
-                    result.data[i+2] = 0xff;
-                    result.data[i+3] = 0xff;
+                    resultData[i] = 0x00;
+                    resultData[i+1] = 0xff;
+                    resultData[i+2] = 0xff;
+                    resultData[i+3] = 0x00;
                 }
             }
-            result.pack().pipe(fs.createWriteStream('result.png')).on('close', function(){
-                console.log('Result done!');
-            });
+            result.data = new Buffer(resultData);
+            console.log(result);
+            result.pack().pipe(fs.createWriteStream('result.png'));
         }
 
     }
@@ -76,5 +103,6 @@ var im = new ImageToolbox();
 
 //im.readImage('test/img.png', function(){});
 im.imageDiff('test/img.png','test/img.png');
+
 
 module.exports = ImageToolbox;
